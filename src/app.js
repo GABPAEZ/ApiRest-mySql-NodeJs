021f495c1476c3370f90fd78b5a0c75e6edd6c5c
Gabriel Paez <gabpaez@gmail.com>
2022-11-07 23:29:57 -0300

    add morgan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import employeesRoutes from "./routes/employees.routes.js";
+import morgan from 'morgan';
+import employeesRoutes from './routes/employees.routes.js';
 import indexRoutes from './routes/index.routes.js';
 
 
@@ -7,7 +8,7 @@ const app = express();
 
 //tiene que estar antes de las rutas es muy importante
 app.use(express.json());
-
+app.use(morgan('tiny'));
 
 //Routes
 
@@ -16,7 +17,7 @@ app.use(indexRoutes);
 
 // Sino encuentra esas rutas avisa ue no existe
 app.use((req, res, next) => {
-    res.status(404).json({message: 'Enpoint not found'});
-})
+  res.status(404).json({ message: 'Enpoint not found' });
+});
 
 export default app;
